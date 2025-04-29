343a0f303fb166944f472d9221d36db9f2a714f6
Daichi44 <DaichiSugawara44@gmail.com>
2025-04-29 21:22:27 +0900

    feat: front側のファイル移動、sass導入

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,12 +1,13 @@
-import './globals.css';
+import '../styles/globals.scss';
 import AppProvider from '../provider/AppProvider';
+import { ReactNode } from 'react';
 
 export const metadata = {
   title: 'Task Timer',
   description: 'Time tracking app',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
